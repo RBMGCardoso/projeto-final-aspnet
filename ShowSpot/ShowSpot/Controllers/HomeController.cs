027b8975943a6ca1c68e21fa1194446169c550f3
Xyrsto <rodrigtomas@gmail.com>
2023-06-01 11:47:43 +0100

    API está a funcionar na main page. HTML tem de ser feito na secção de script para podermos consumir a API

--- ShowSpot/ShowSpot/Controllers/HomeController.cs
+++ ShowSpot/ShowSpot/Controllers/HomeController.cs
@@ -29,34 +29,6 @@ public class HomeController : Controller
         return conteudos;
     }
 
-    public List<dynamic> getFilmes()
-    {
-        var result = _context.Conteudos
-            .Where(c => c.Tipo == false)
-            .OrderByDescending(c => c.Id)
-            .Take(50)
-            .Select(c => new 
-            {
-                Id = c.Id,
-                Nome = c.Nome,
-                ImgUrl = c.ImgUrl,
-                Sinopse = c.Sinopse,
-                Rating = c.Rating,
-                Tipo = c.Tipo,
-                Runtime = c.Runtime,
-                AnoLancamento = c.AnoLancamento,
-                LinkTrailer = c.LinkTrailer,
-                // Set Tag property separately
-                Tag = _context.ConteudoTags
-                    .Where(ct => ct.ConteudoFK == c.Id)
-                    .Join(_context.Tags, ct => ct.TagFK, t => t.Id, (ct, t) => t.Nome)
-                    .FirstOrDefault()
-            })
-            .ToList();
-
-        return result;
-    }
-
     public IActionResult Index()
     {
         var conteudos = ConteudosList();
