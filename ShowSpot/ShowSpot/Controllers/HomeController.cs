0c1e2dbef53d2089abd31bb0cc627bb77e9a7055
Xyrsto <rodrigtomas@gmail.com>
2023-06-01 11:21:46 +0100

    Main screen está metade feito

--- ShowSpot/ShowSpot/Controllers/HomeController.cs
+++ ShowSpot/ShowSpot/Controllers/HomeController.cs
@@ -1,5 +1,8 @@
 ﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.FileProviders.Composite;
+using ShowSpot.Data;
 using ShowSpot.Models;
 
 namespace ShowSpot.Controllers;
@@ -7,18 +10,70 @@ namespace ShowSpot.Controllers;
 public class HomeController : Controller
 {
     private readonly ILogger<HomeController> _logger;
+    private readonly ApplicationDbContext _context;
+    private readonly UserManager<IdentityUser> _userManager;
+    private readonly SignInManager<IdentityUser> _signInManager;
 
-    public HomeController(ILogger<HomeController> logger)
+    public HomeController(ILogger<HomeController> logger, ApplicationDbContext context, UserManager<IdentityUser> userManager, SignInManager<IdentityUser> signInManager)
     {
         _logger = logger;
+        _context = context;
+        _userManager = userManager;
+        _signInManager = signInManager;
+    }
+
+    public List<Conteudos> ConteudosList()
+    {
+        var conteudos = _context.Conteudos.ToList();
+
+        return conteudos;
+    }
+
+    public List<dynamic> getFilmes()
+    {
+        var result = _context.Conteudos
+            .Where(c => c.Tipo == false)
+            .OrderByDescending(c => c.Id)
+            .Take(50)
+            .Select(c => new 
+            {
+                Id = c.Id,
+                Nome = c.Nome,
+                ImgUrl = c.ImgUrl,
+                Sinopse = c.Sinopse,
+                Rating = c.Rating,
+                Tipo = c.Tipo,
+                Runtime = c.Runtime,
+                AnoLancamento = c.AnoLancamento,
+                LinkTrailer = c.LinkTrailer,
+                // Set Tag property separately
+                Tag = _context.ConteudoTags
+                    .Where(ct => ct.ConteudoFK == c.Id)
+                    .Join(_context.Tags, ct => ct.TagFK, t => t.Id, (ct, t) => t.Nome)
+                    .FirstOrDefault()
+            })
+            .ToList();
+
+        return result;
     }
 
     public IActionResult Index()
     {
-        return View();
+        var conteudos = ConteudosList();
+        return View(conteudos);
     }
 
     public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    public IActionResult Filmes()
+    {
+        return View();
+    }
+
+    public IActionResult Series()
     {
         return View();
     }
---
+++ ShowSpot/ShowSpot/Views/Conteudos/Filmes.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ShowSpot.Views.Conteudos
+{
+    public class FilmesModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
